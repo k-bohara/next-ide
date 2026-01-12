fd8d473896fda20ea15bd2be313b01fb1dbb8025
Kaman Bohara <kamanbohara10@Kamans-MacBook-Air.local>
2026-01-12 13:13:14 +0545

    error tracking using sentry

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,8 +1,12 @@
 "use client"
 import { Button } from "@/components/ui/button"
 import React, { useState } from "react"
+import * as Sentry from "@sentry/nextjs"
+import { useAuth } from "@clerk/clerk-react"
 
 const DemoPage = () => {
+  const { userId } = useAuth()
+
   const [loading, setLoading] = useState(false)
   const [loading2, setLoading2] = useState(false)
 
@@ -21,6 +25,21 @@ const DemoPage = () => {
     })
     setLoading2(false)
   }
+  const handleClientError = () => {
+    Sentry.logger.info("user attempting to click on client function", { userId })
+    throw new Error("Client error: Something went wrong!")
+  }
+
+  const handleApiError = async () => {
+    await fetch("/api/demo/error", { method: "POST" })
+  }
+
+  const handleInngestError = async () => {
+    await fetch("/api/demo/inngest-error", {
+      method: "POST",
+    })
+  }
+
   return (
     <div className="p-8 space-x-5">
       <Button onClick={handleBlocking}>
@@ -28,6 +47,16 @@ const DemoPage = () => {
       </Button>
       <Button onClick={handleBackground}>
         {loading2 ? "Loading..." : "Blocking"}
+      </Button>
+
+      <Button variant={"destructive"} onClick={handleClientError}>
+        Client Error
+      </Button>
+      <Button variant={"destructive"} onClick={handleApiError}>
+        API Error
+      </Button>
+      <Button variant={"destructive"} onClick={handleInngestError}>
+        Inngest Error
       </Button>
     </div>
   )
